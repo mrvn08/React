bb7685194ba7e1a8a260f9118807f8ae0412aba2
Marvin <marvinlimson2007@gmail.com>
2018-06-03 16:09:40 +0100

    Added in yarn, babel, and react and got it to compile

---
+++ Indecision/src/app.js
@@ -0,0 +1,24 @@
+console.log('App.js is running');
+
+// JSX - JavaScript XML
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Marvin Limson</h1>
+        <p>Age: 27</p>
+        <p>Location: Barnet</p>
+    </div>
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
