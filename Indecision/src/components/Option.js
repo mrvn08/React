8b0299a5deb33b0b431b6fc2f265ae3c22263d0b
Marvin <marvinlimson2007@gmail.com>
2018-06-11 22:02:24 +0100

    Added more styles

--- Indecision/src/components/Option.js
+++ Indecision/src/components/Option.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Option = (props) => ( 
     <div>
         Option: {props.optionText}
-        <button 
+        <button
+            className="button button--link"
             onClick={(e) => {
                 props.handleDeleteOption(props.optionText);
             }}
--- Indecision/src/components/Options.js
+++ Indecision/src/components/Options.js
@@ -3,17 +3,27 @@ import Option from  './Option';
 
 const Options = (props) => (
     <div>
-        <button onClick={props.handleDeleteOptions}>Remove All</button>
-        {props.options.length === 0 && <p>Please add an option to get started.</p>}
-        {
-            props.options.map((option) => (
-                <Option 
-                    key={option} 
-                    optionText={option} 
-                    handleDeleteOption={props.handleDeleteOption}    
-                />
-            ))
-        }
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
+            <button
+                className="button button--link" 
+                onClick={props.handleDeleteOptions}
+            >
+                Remove All
+            </button>
+        </div>
+        <div className="widget__list">
+            {props.options.length === 0 && <p className="widget__message">Please add an option to get started.</p>}
+            {
+                props.options.map((option) => (
+                    <Option 
+                        key={option} 
+                        optionText={option} 
+                        handleDeleteOption={props.handleDeleteOption}    
+                    />
+                ))
+            }
+        </div>
     </div>
 );
 
