5aa85b4c81e11484afd97c46256bc18f2fa9134d
Marvin Limson <marvinlimson2007@gmail.com>
2018-06-11 16:21:14 +0100

    Added normalize css. And showed how to include css as well in webpack

--- Indecision/webpack.config.js
+++ Indecision/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/
         }, {
-            test: /\.scss$/,
+            test: /\.s?css$/,
             use: [
                 'style-loader',
                 'css-loader',
