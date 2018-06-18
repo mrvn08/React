8e8d6f713c5428edfe21168c5f4ed183dda189e5
Marvin Limson <marvinlimson2007@gmail.com>
2018-06-18 16:50:20 +0100

    Used spread operators to handle object property modification

--- Expensify/src/playground/redux-expensify.js
+++ Expensify/src/playground/redux-expensify.js
@@ -20,7 +20,17 @@ const removeExpense = ({ id } = {}) => ({
 });
 
 // Edit Expense
+const editExpense = ( id = "", updates ) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
 // Set Text Filter
+const setTextFilter = ( text = "" ) => ({
+    type: 'SET_TEXT',
+    text
+});
 // Sort by date
 // Sort by amount
 // Set start date
@@ -41,7 +51,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
             //return [action.expense, ...state];
         case 'REMOVE_EXPENSE':
             return  state.filter((singleExpense) => singleExpense.id != action.id );
-
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if(expense.id === action.id){
+                    return {
+                        ...expense,
+                        ...action.updates
+                    }
+                } else {
+                    return expense;
+                }
+            })
         default:
             return state;
     }
@@ -56,6 +76,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch(action.type){
+        case 'SET_TEXT':
+            return {
+                ...state,
+                text: action.text
+            };
         default:
             return state;
     }
@@ -77,10 +102,14 @@ store.subscribe(() => {
 
 //Calling the actions
 const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
-store.dispatch(addExpense({ description: 'Coffee', note: 'It was delicious', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', note: 'It was delicious', amount: 100 }));
 
 console.log(expenseOne);
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense( expenseTwo.expense.id, { amount: 800 }));
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
+
 
 
 const demoState = {
@@ -100,3 +129,23 @@ const demoState = {
 };
 
 //In this scenario it is best to split the reducers and use Combine Reducers
+
+const user  = {
+   name: 'Jen',
+   age: 26   
+};
+
+console.log({
+    ...user,
+    location: 'Philadelphia',
+    age: 27
+});
+
+//Note that this is acheived with babel plugin transform object rest spread
+//location is added and age is overriden, unless it's put before the spread operator
+
+console.log({
+    age: 27,
+    ...user,
+    location: 'Philadelphia',
+});
