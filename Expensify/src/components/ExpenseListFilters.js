95c6be20004f7733665abfe8e7c3201591308f22
Marvin Limson <marvinlimson2007@gmail.com>
2018-06-26 17:57:11 +0100

    Finished adding test for edit filters

--- Expensify/src/components/ExpenseListFilters.js
+++ Expensify/src/components/ExpenseListFilters.js
@@ -3,19 +3,27 @@ import { connect } from 'react-redux';
 import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters';
 import { DateRangePicker } from 'react-dates';
 
-class ExpenseListFilters extends React.Component{
+export class ExpenseListFilters extends React.Component{
 
     state = {
         calendarFocused: null
     };
 
     onDatesChange = ({startDate, endDate}) => {
-        this.props.dispatch(setStartDate(startDate));
-        this.props.dispatch(setEndDate(endDate));
+        this.props.setStartDate(startDate);
+        this.props.setEndDate(endDate);
     };
 
     onFocusChange = (calendarFocused) => {
         this.setState(() => ({ calendarFocused }));
+    };
+
+    onTextChange = (e) => {
+        this.props.setTextFilter(e.target.value);
+    };
+
+    onSortChange = (e) => {
+        e.target.value == "date" ? this.props.sortByDate() : this.props.sortByAmount();
     };
 
     render(){
@@ -24,13 +32,11 @@ class ExpenseListFilters extends React.Component{
                 <input 
                     type="text" 
                     value={this.props.filters.text} 
-                    onChange={(e) => this.props.dispatch(setTextFilter(e.target.value))}
+                    onChange={this.onTextChange}
                 />
                 <select 
                     value={this.props.filters.sortBy} 
-                    onChange={(e) => {
-                        e.target.value == "date" ? this.props.dispatch(sortByDate()) : this.props.dispatch(sortByAmount());
-                    }}
+                    onChange={this.onSortChange}
                     >
                     <option value="date">Date</option>
                     <option value="amount">Amount</option>
@@ -50,10 +56,15 @@ class ExpenseListFilters extends React.Component{
     }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        filters: state.filters
-    };
-};
+const mapDispatchToProps = (dispatch) => ({
+    setTextFilter: (text) => dispatch(setTextFilter(text)),
+    setStartDate: (date) => dispatch(setStartDate(date)),
+    setEndDate: (date) => dispatch(setEndDate(date)),
+    sortByAmount: () => dispatch(sortByAmount()),
+    sortByDate: () => dispatch(sortByDate())
+});
+const mapStateToProps = (state) => ({
+    filters: state.filters
+});
 
-export default connect(mapStateToProps)(ExpenseListFilters);
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
--- Expensify/src/tests/components/ExpenseListFilters.test.js
+++ Expensify/src/tests/components/ExpenseListFilters.test.js
@@ -1,3 +1,78 @@
-test('', () => {
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpenseListFilters} from '../../components/ExpenseListFilters';
+import { filters, altFilters} from '../fixtures/filters';
 
+let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
+
+beforeEach(() => {
+    setTextFilter = jest.fn();
+    sortByDate = jest.fn();
+    sortByAmount = jest.fn();
+    setStartDate = jest.fn();
+    setEndDate = jest.fn();
+    wrapper = shallow(
+        <ExpenseListFilters
+            filters = {filters} 
+            setTextFilter  = {setTextFilter}
+            sortByDate = {sortByDate}
+            sortByAmount = {sortByAmount}
+            setStartDate = {setStartDate}
+            setEndDate = {setEndDate}
+        />
+    );
+});
+
+test('Should render filters correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should render filters with alternate data', () => {
+    wrapper.setProps({ filters: altFilters });
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should handle text change', () => {
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.find('input').simulate('change', { target: { value: 'Some text'}});
+    expect(setTextFilter).toHaveBeenLastCalledWith('Some text');
+    
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should sort by date', () => {
+    wrapper.setProps({ filters: altFilters });
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.find('select').simulate('change', { target: { value: 'date'}});
+    expect(sortByDate).toBeCalled();
+
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should sort by amount', () => {
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.find('select').simulate('change', { target: { value: 'amount'}});
+    expect(sortByAmount).toBeCalled();
+
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should handle date changes', () => {
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.find('DateRangePicker').prop('onDatesChange')({ startDate: altFilters.startDate, endDate: altFilters.endDate });
+    expect(setStartDate).toHaveBeenLastCalledWith( altFilters.startDate );
+    expect(setEndDate).toHaveBeenLastCalledWith( altFilters.endDate );
+    
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should hand date focus changes', () => {
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.find('DateRangePicker').prop('onFocusChange')(true);
+    expect(onFocusChange).toHaveBeenLastCalledWith({ calendarFocused: true })
 });
