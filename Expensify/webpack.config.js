b8a004d8a6a7a697218312c874f75e11a0f836fa
Marvin Limson <marvinlimson2007@gmail.com>
2018-06-28 12:19:32 +0100

    Added webpack plugin for extracting  css files from app.js. Setup source maps for css

--- Expensify/webpack.config.js
+++ Expensify/webpack.config.js
@@ -2,13 +2,15 @@
 //webpack.js.org
 //This is required to run path.join
 const path = require('path');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 //Entry Point -> Output Point
 
 //__dirname contains the path to the project/public
 //console.log(path.join(__dirname,'public'));
 module.exports = (env) => {
     const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
     console.log('env', env);
     return {
         entry: './src/app.js',
@@ -23,14 +25,28 @@ module.exports = (env) => {
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }, 
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
             }]
         },
-        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: { 
             contentBase: path.join(__dirname,'public'),
             historyApiFallback: true
